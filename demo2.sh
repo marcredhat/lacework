8a2a7a09b662a6e05d3903e32e3f320fd8514feb
Marc Chisinevski <mchisine@redhat.com>
2020-07-17 13:52:29 -0700

    Update demo2.sh

--- demo2.sh
+++ demo2.sh
@@ -13,4 +13,8 @@ oc get pods
 
 printf  "***1) In Sysdig, go to Secure/ Activity Audit / select Lacework containers \n\n"
 
-printf  "***2) In Sysdig, go to Secure/ Policies / Rules Library / Add Rule \n\n"
+printf  "***2) In Sysdig, go to Secure/ Policies / Runtime Policies / select or create Policy \n\n"
+
+printf  "***3) In Sysdig, go to Secure/ Policies / Rules Library / select or create Rule \n\n"
+
+printf  "***4) In Sysdig, go to Secure/ Benchmarks \n\n"
