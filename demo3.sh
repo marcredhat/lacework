e930e077745f2732f9ddfdc7f77e85ee60172ff5
Marc Chisinevski <mchisine@redhat.com>
2020-07-18 19:27:51 -0700

    Update demo3.sh

--- demo3.sh
+++ demo3.sh
@@ -7,7 +7,7 @@ printf "\n\n ***** 4) Run commands inside Lacework container using podman *****\
 
 git clone https://github.com/lacework/go-sdk.git
 cd go-sdk/
-podman  login -u="marcf5" -p="<encrypted password from quay.io" quay.io
+podman  login -u="marcf5" -p="<encrypted password from quay.io>" quay.io
 
 rm -rf Dockerfile
 wget https://raw.githubusercontent.com/lacework/go-sdk/master/cli/images/ubi-8/Dockerfile
@@ -19,16 +19,16 @@ printf "\n\n ***** pushing localhost/lacework to quay.io/marcf5/lacework *****\n
 podman push localhost/lacework:latest  quay.io/marcf5/lacework
 podman images
 #https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/building_running_and_managing_containers/working-with-containers_building-running-and-managing-containers
-#podman run --name=marclaceworkpodman -it localhost/lacework:latest /bin/bash
-laceworkcontainer=`podman container ls  | grep lacework |  awk '{print $1'}`
+podman run --name=demolaceworkpodman2 -it localhost/lacework:latest &
+sleep 6
+laceworkcontainer=`podman container ls  | grep demolaceworkpodman2 |  awk '{print $1'}`
 
 #To create a set of API keys, log in to your Lacework account via WebUI and navigate to Settings > API Keys and click + Create New.
 #Enter a name for the key and an optional description, then click Save.
 #To get the secret key, download the generated API key file.
 
 printf "\n\n ***** Configuring Lacework and authenticating *****\n\n"
-printf "\n\n ***** Press <ENTER> multiple times *****\n\n"
-podman exec -i $laceworkcontainer '/bin/sh' -c 'lacework configure --account "<account>" --api_key "<API key>" --api_secret "<API secret>"'
+podman exec -i  $laceworkcontainer '/bin/sh' -c 'lacework configure --account "<account>" --api_key "<API key>" --api_secret "<API secret>"'
 
 printf "\n\n ***** Run commands inside Lacework container using podman *****\n\n"
 podman exec -i $laceworkcontainer '/bin/sh' -c 'lacework events list | more'
